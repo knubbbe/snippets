5bc1fe59be93f5e5e3a63efb36da72a8192f6284
Marcus Melvinson <knubbbe@gmail.com>
2016-05-26 11:52:39 +0200

    Create acf-gallery-wmpl-fix.php

---
+++ acf-gallery-wmpl-fix.php
@@ -0,0 +1,24 @@
+/*
+ * PUT THIS IN functions.php
+ */
+add_filter( 'acf/load_value/type=gallery', 'my_acf_load_translated_attachment', 10, 3 );
+add_filter( 'acf/load_value/type=image', 'my_acf_load_translated_attachment', 10, 3 );
+
+function my_acf_load_translated_attachment($value, $post_id, $field) {
+	$newValue = $value;
+
+	// Make sure we are using WPML
+	if ( function_exists('icl_object_id') ) {
+		// Galleries come in arrays
+		if ( is_array($value) ) {
+			$newValue = array();
+			foreach ($value as $key => $id) {
+				$newValue[$key] = icl_object_id($id, 'attachment');
+			}
+		} else {
+			$newValue = icl_object_id($value, 'attachment');
+		}
+	}
+
+	return $newValue;
+}
